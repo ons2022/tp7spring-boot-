214f116651b23b5fef7142cb08d3b722a4a692bf
ons2022 <onsismailhajri@gmail.com>
2023-12-06 11:34:53 +0100

    initial

---
+++ src/main/java/tp7spring/boot/tp7/Tp7springBootApplication.java
@@ -0,0 +1,13 @@
+package tp7spring.boot.tp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp7springBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp7springBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tp7spring/boot/tp7/Tp7springBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tp7spring.boot.tp7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp7springBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
